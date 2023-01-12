2489bd6a2b52f9a605b52b0e2c7c192edf54c536
GlauberBalsani <glauber_29@hotmail.com>
2023-01-11 23:52:01 -0300

    add ui

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,13 +3,13 @@ package boardgame;
 public class Position {
 	
 	private int row;
-	private int columm;
+	private int column;
 	
 	
 	public Position(int row, int columm) {
 		super();
 		this.row = row;
-		this.columm = columm;
+		this.column = column;
 	}
 
 
@@ -23,20 +23,19 @@ public class Position {
 	}
 
 
-	public int getColumm() {
-		return columm;
+	public int getColumn() {
+		return column;
 	}
 
 
-	public void setColumm(int columm) {
-		this.columm = columm;
+	public void setColumn(int column) {
+		this.column = column;
 	}
 	
 	
 	@Override
 	public String toString() {
-		
-		return row + ", " + columm;
+		return row + ", " + column;
 	}
 	
 
