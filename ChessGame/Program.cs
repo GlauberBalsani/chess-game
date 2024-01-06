19642f423dcc3c767aa8e051194a75ebb8614545
GlauberBalsani <glauber_29@hotmail.com>
2024-01-05 21:51:44 -0300

    tratando erros

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -2,14 +2,18 @@
 using ChessGame.Domain.Models;
 using ChessGame.Domain.Models.Xadrez;
 
+try
+{
+    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
+    tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+    tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 0));
+    tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+    tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
 
-
-Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0,0));
-tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0,0));
-tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1,3));
-tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2,4));
-
-Tela.ImprimirTabuleiro(tabuleiro);
+    Tela.ImprimirTabuleiro(tabuleiro);
+}
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message.ToString());
+}
